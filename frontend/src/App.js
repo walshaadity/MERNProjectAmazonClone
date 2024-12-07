378b94bcca496ed4eccd19ceaaf389c613252174
Walsha Adity <walshaadity@gmail.com>
2024-12-07 19:39:45 +0530

    Create NodeJS Server

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,22 @@
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 function App() {
   return (
-    <div>
-      <header>
-        <a href="/">amazonW</a>
-      </header>
-      <main>
-        List Products
-      </main>
-    </div>
+    <BrowserRouter>
+      <div>
+        <header>
+          <Link to="/">amazona</Link>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/product/:slug" element={<ProductScreen />} />
+            <Route path="/" element={<HomeScreen />} />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 
