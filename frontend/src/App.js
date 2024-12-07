a4eea7d83bdd6e0139652c2e9f15da826284e321
Walsha Adity <walshaadity@gmail.com>
2024-12-07 12:40:54 +0530

    Mern-amazonW

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">amazonW</a>
+      </header>
+      <main>
+        List Products
+      </main>
+    </div>
+  );
+}
+
+export default App;
